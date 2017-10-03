257b5bd3c9a577bd08ab945efb793abc3d41e2a9
SloRunner <slo.runner.samp@gmail.com>
2017-10-03 17:21:21 +0300

    Use JavaScript Standard Style (https://standardjs.com)

--- nodejs/grab.js
+++ nodejs/grab.js
@@ -1,22 +1,22 @@
-var mysql      = require('mysql');
+var mysql = require('mysql')
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'kippo',
-  password : 'Kippo-DB-pass',
-  database : 'kippo'
-});
+  host: 'localhost',
+  user: 'kippo',
+  password: 'Kippo-DB-pass',
+  database: 'kippo'
+})
 
-//-------------------------GENERATOR CODE------------------------
-connection.connect();
-connection.query('select username, password from auth;', function(err, rows, fields) {
-  if (!err){
-	for (var i = rows.length - 1; i >= 0; i--) {
-		console.log(rows[i].username + ':0:' + rows[i].password);
-	};
-  }else{
-  	console.log('Error while performing Query.');
+// -------------------------GENERATOR CODE------------------------
+connection.connect()
+connection.query('select username, password from auth;', function (err, rows, fields) {
+  if (!err) {
+    for (var i = rows.length - 1; i >= 0; i--) {
+      console.log(rows[i].username + ':0:' + rows[i].password)
+    };
+  } else {
+    console.log('Error while performing Query.')
   };
-});
+})
 
-connection.end();
-//----------------------------END OF CODE------------------------
+connection.end()
+// ----------------------------END OF CODE------------------------
